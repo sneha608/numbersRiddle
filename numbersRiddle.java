f971102528abc5a76b9ae0ba708c7e9bfdae503a
sneha608 <sharma9543@mydusd.org>
2026-09-08 11:35:44 -0700

    starting #
    Fix syntax errors and implement number doubling logic.

--- numbersRiddle.java
+++ numbersRiddle.java
@@ -1,5 +1,11 @@
 public class numbersRiddle {
   public static void main(String[] args) {
-      
+
+    int chosenNumber
+    System.out.println("The chosen number is " + chosenNumber)
+
+    chosenNumber *= 2
+    System.out.println("The number doubled is " + chosenNumber)
+    
   }
 }

1f9c72c2d9d2722f6677acb84495255b5149f665
bradthezhonger <zhong.brad@gmail.com>
2026-09-09 11:09:34 -0700

    cleaned up code, put results in comments where variable initialization is

--- numbersRiddle.java
+++ numbersRiddle.java
@@ -1,27 +1,27 @@
 public class numbersRiddle {
   public static void main(String[] args) {
 
-    int positiveInteger = 5;
-    int negativeInteger = -1;
-    int zeroInteger = 0;
-    int oneInteger = 1;
-    double positiveDouble = 7.9;
-    double negativeDouble = -3.7;
-  
+    int positiveInteger = 5; // result is 3.0
+    int negativeInteger = -1; // result is 3.0
+    int zeroInteger = 0; // result is 3.0
+    int oneInteger = 1; // result is 3.0
+    double positiveDouble = 7.9; // result is 3.0
+    double negativeDouble = -3.7; // result is 3.0
 
-    double chosenNumber;
+    double chosenNumber = positiveInteger; 
     System.out.println("The chosen number is " + chosenNumber);
     double originalchosenNumber = chosenNumber;
+    
     chosenNumber *= 2;
     System.out.println("The number doubled is " + chosenNumber);
 
     chosenNumber +=6;
-    System.out.println("Adding 6 to your number yields: "+chosenNumber);
+    System.out.println("Adding 6 to the result yields: " + chosenNumber);
 
-    chosenNumber = chosenNumber/2;
-    System.out.println("The number divided by 2 is " + chosenNumber);
+    chosenNumber /= 2;
+    System.out.println("The result divided by 2 is " + chosenNumber);
 
     chosenNumber -= originalchosenNumber;
-    System.out.println("The number subtracted by the chosen number is" + chosenNumber);
+    System.out.println("The result subtracted by the original chosen number and hence, the final number is" + chosenNumber);
   }
 }
